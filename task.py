49b1f63a6a6e34ffe027503abeeaf3cf58293431
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 12:16:39 +0800

    Implement initial task processing with Celery and Redis integration

---
+++ task.py
@@ -0,0 +1,17 @@
+from celery import Celery
+from time import sleep
+
+app = Celery('tasks', broker='redis://localhost:6379')
+
+@app.task
+def  process(x, y):
+    i = 0
+    while i < 5:
+        sleep(1)
+        i += 1
+        print("Processing...")
+        
+    return x**2 + y**2 
+
+if __name__ == '__main__':
+    print(process(2, 3))
